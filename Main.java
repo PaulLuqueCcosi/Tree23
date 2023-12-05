ff1563dd8eb0b0f15efa91c3f88d05c404a2f6a1
PoolLuque <pluquec@unsa.edu.pe>
2023-12-04 21:49:10 -0500

    Agregando en el main

--- Main.java
+++ Main.java
@@ -8,8 +8,16 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Starting");
         DTATree23 treeInteger = new Tree23<Integer>();
-        TestTree23Integer test = new TestTree23Integer(treeInteger);
-        test.testAll();
+        // TestTree23Integer test = new TestTree23Integer(treeInteger);
+        // test.testAll();
+        treeInteger.insert(10);
+        treeInteger.insert(5);
+        treeInteger.insert(15);
+        treeInteger.insert(20);
+        System.out.println(treeInteger.inOrder());
+
+
+        treeInteger.insert(5);
         System.out.println("Finished");
 
     }
